ad18b187e4c5a838f2504ba2d6a5ef961d11af6f
Ivan Sosnin <vansosnin@gmail.com>
2018-07-14 23:45:12 +0500

    init firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,47 @@
+import firebase from 'firebase';
+import config from '../config.json';
+
+let instance = null;
+
+const PROJECT_ID = config['firebase-project-id'];
+const config = {
+    apiKey: config['firebase-api-key'],
+    projectId: PROJECT_ID,
+    databaseURL: `https://${PROJECT_ID}.firebaseio.com/`,
+    authDomain: `${PROJECT_ID}.firebaseapp.com`,
+    storageBucket: `${PROJECT_ID}.appspot.com`,
+};
+
+export default class Firebase {
+    constructor() {
+        if (instance) {
+            return instance;
+        }
+
+        this._firebase = firebase;
+        this._firebase.initializeApp(config);
+        this._db = this._firebase.database();
+        this._ref = this._db.ref();
+
+        instance = this;
+        return instance;
+    }
+
+    static instance() {
+        return new Firebase();
+    }
+
+    get auth() {
+        return this._firebase.auth();
+    }
+
+    get providers() {
+        return {
+            google: new this._firebase.auth.GoogleAuthProvider(),
+        };
+    }
+
+    get db() {
+        return this._db;
+    }
+}
